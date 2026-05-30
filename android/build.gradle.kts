8808ae7b8488155815554d00269a76f053739a3f
quagntam <quangtam208005@gmail.com>
2026-05-30 14:14:49 +0700

    Update localization and employer job posting

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 allprojects {
     repositories {
         google()
@@ -17,6 +20,20 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        // Some Flutter plugins still compile Java with 1.8/11 while Kotlin defaults to a newer target.
+        // Align Kotlin bytecode target with the paired Java compile task for each variant.
+        val javaTaskName = name.replace("Kotlin", "JavaWithJavac")
+        val pairedJavaTask = project.tasks.findByName(javaTaskName)
+        if (pairedJavaTask is org.gradle.api.tasks.compile.JavaCompile) {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.fromTarget(pairedJavaTask.targetCompatibility))
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
